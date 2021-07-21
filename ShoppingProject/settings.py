961a8978d269acee54a640e98b8aba23ba68b90a
kiteB <mmy789@sch.ac.kr>
2021-07-21 16:38:16 +0900

    ShoppingProject - settings.py - secret key 관련 코드 수정

--- ShoppingProject/settings.py
+++ ShoppingProject/settings.py
@@ -29,6 +29,11 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # secrets.json 파일에서 SECRET_KEY 가져오기    
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+    
 def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
