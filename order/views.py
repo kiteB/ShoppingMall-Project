b8ed17c600e50d36fe68eb0c4a506611a37860ee
kiteB <mmy789@sch.ac.kr>
2021-07-21 23:43:35 +0900

    README.md 추가

--- order/views.py
+++ order/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from account.models import User
+from product.models import Product
+from .models import *
